c205b384bddacb08f021111fe47f14fcb2d7df4a
Mohsin Javed <mohsin9042002@gmail.com>
2025-05-13 23:59:28 +0500

    first commit

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'rimshabatool.0@outlook.com',
+    phone: '+8801608797655',
+    address: 'Rawalpindi, Pakistan',
+    github: 'https://github.com/said7388',
+    facebook: 'https://www.facebook.com/abusaid.riyaz/',
+    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
+    twitter: 'https://twitter.com/said7388',
+    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+    devUsername: "said7388"
+}
---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "Rimsha Batool",
+  profile: '/profile.png',
+  designation: "Software Developer",
+  description: "My name is Rimsha Batool.  React Native Developer with hands-on experience in React Native, JavaScript, TypeScript, and specializing in building cross-platform mobile applications. Proficient in frontend and back-end development with a strong understanding of API integrations, state management, and performance optimization. Skilled in collaborative teamwork and delivering user-centric applications with seamless functionality. Passionate about leveraging emerging technologies to create innovative digital solutions and committed to continuous learning to stay ahead in the tech industry.",
+  email: 'abusaid7388@gmail.com',
+  phone: '+8801608797655',
+  address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
+  github: 'https://github.com/said7388',
+  facebook: 'https://www.facebook.com/abusaid.riyaz/',
+  linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
+  twitter: 'https://twitter.com/said7388',
+  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+  leetcode: "https://leetcode.com/said3812/",
+  devUsername: "said7388",
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+}
